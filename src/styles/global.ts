6ce952d43ec48aa93ea4eee515c50adf136722df
imddoy <imddoy@duksung.ac.kr>
2024-07-03 20:18:40 +0900

    feat: 글로벌 스타일 설정

---
+++ src/styles/global.ts
@@ -0,0 +1,61 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const global = createGlobalStyle`
+
+    ${reset}
+    
+    * {
+    box-sizing: border-box;
+    }
+    
+    body {
+        /* background-color: ${({ theme }) => theme.colors.gray900};; */
+    }
+
+    html {
+    -webkit-touch-callout: none;
+    -webkit-tap-highlight-color:rgb(0 0 0 / 0%);
+    scroll-behavior: smooth;
+
+    font-size: 62.5%;
+    user-select: none;
+
+    background-color: ${({ theme }) => theme.colors.gray900};;
+
+    }
+    ul, li {
+    padding-left: 0;
+    list-style: none;
+    }
+
+    a {
+    color: inherit;
+
+    text-decoration: none;
+    }
+
+    input, button {
+    outline: none;
+
+    border: none;
+    background-color: transparent;
+    }
+
+    button {
+    cursor: pointer;
+
+    padding: 0;
+    }
+
+    input {
+    appearance: none;
+
+    &:focus {
+        outline: none;
+    }
+    }
+
+`;
+
+export default global;
