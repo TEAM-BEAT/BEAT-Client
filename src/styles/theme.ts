027940c9558adc77b7f5346df717a7393931eb51
imddoy <imddoy@duksung.ac.kr>
2024-07-03 20:04:53 +0900

    fix: 깃 리베이스 충돌해결

---
+++ src/styles/theme.ts
@@ -0,0 +1,47 @@
+const theme = {
+  color: {
+    white: "#FFFFFF",
+    black: "#0F0F0F",
+    main_pink_400: "#FF006B",
+    // pink
+    pink_0: "#FFEAF2",
+    pink_100: "#FFCFE3",
+    pink_200: "#FF97C2",
+    pink_300: "#FF4F98",
+    pink_400: "#FB247F",
+    pink_500: "#E30964",
+    pink_600: "#B80450",
+    pink_700: "#940742",
+    pink_800: "#6F0531",
+    pink_900: "#4D0322",
+    // gray
+    gray_0: "#F4F4F4",
+    gray_100: "#EEEEEE",
+    gray_200: "#CCCCCC",
+    gray_300: "#B2B2B2",
+    gray_400: "#939393",
+    gray_500: "#797979",
+    gray_600: "#626262",
+    gray_700: "#454545",
+    gray_800: "#2A2A2A",
+    gray_900: "#1B1B1B",
+    // purple
+    purple_0: "#F6EEFF",
+    purple_100: "#E2CCFF",
+    purple_200: "#B880FF",
+    pruple_300: "#9D51FF",
+    purple_400: "#811FFF",
+    purple_500: "#6D08EF",
+    purple_600: "#5B09C3",
+    purple_700: "#450795",
+    purple_800: "#380578",
+    purple_900: "#24034E",
+    // semantic
+    red: "#FF4141",
+    green: "#1ED45A",
+  },
+};
+
+export type ColorType = typeof theme.color;
+
+export default theme;
