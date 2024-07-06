dc930660127b6f550e277d91584891e4e9442022
ocahs9 <piorag99@gmail.com>
2024-07-07 00:17:15 +0900

    fix: 테스트 케이스 제거- App.tsx 원상복구

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import Button from "@components/commons/Button/Button";
 import router from "@routes/Router";
 import GlobalStyle from "@styles/global";
 import theme from "@styles/theme";
@@ -6,26 +5,9 @@ import { RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 
 function App() {
-  const handleOnClick = () => {
-    console.log("click!");
-  };
-
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Button onClick={handleOnClick} varient="primary" size="xlarge" isDisabled={true}>
-        테스트1
-      </Button>
-      <Button onClick={handleOnClick} varient="primary" size="xlarge" isDisabled={false}>
-        테스트2
-      </Button>
-      <Button onClick={handleOnClick} varient="blue" size="small" isDisabled={true}>
-        테스트2
-      </Button>
-      <Button onClick={handleOnClick} varient="blue" size="xlarge" isDisabled={false}>
-        테스트2
-      </Button>
-
       <RouterProvider router={router} />
     </ThemeProvider>
   );
