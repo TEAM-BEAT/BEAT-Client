f61f2b219676173a9da5751d22964d419ee2cfd7
ocahs9 <piorag99@gmail.com>
2024-07-07 00:11:48 +0900

    feat: onClick 함수 연결 가능하도록 버튼 구현

--- src/App.tsx
+++ src/App.tsx
@@ -6,112 +6,18 @@ import { RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 
 function App() {
+  const handleOnClick = () => {
+    console.log("click!");
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      {/* Primary Buttons */}
-      <h2>Primary</h2>
-      <Button varient="primary" size="xlarge" isDisabled={false}>
-        Primary XL Enabled
+      <Button onClick={handleOnClick} varient="primary" size="xlarge" isDisabled={true}>
+        테스트1
       </Button>
-      <Button varient="primary" size="xlarge" isDisabled={true}>
-        Primary XL Disabled
-      </Button>
-      <Button varient="primary" size="large" isDisabled={false}>
-        Primary L Enabled
-      </Button>
-      <Button varient="primary" size="large" isDisabled={true}>
-        Primary L Disabled
-      </Button>
-      <Button varient="primary" size="medium" isDisabled={false}>
-        Primary M Enabled
-      </Button>
-      <Button varient="primary" size="medium" isDisabled={true}>
-        Primary M Disabled
-      </Button>
-      <Button varient="primary" size="small" isDisabled={false}>
-        Primary S Enabled
-      </Button>
-      <Button varient="primary" size="small" isDisabled={true}>
-        Primary S Disabled
-      </Button>
-      <Button varient="primary" size="xsmall" isDisabled={false}>
-        Primary XS Enabled
-      </Button>
-      <Button varient="primary" size="xsmall" isDisabled={true}>
-        Primary XS Disabled
-      </Button>
-
-      {/* Line Buttons */}
-      <h2>Line</h2>
-      <Button varient="line" size="xlarge" isDisabled={false}>
-        Line XL Enabled
-      </Button>
-      <Button varient="line" size="xlarge" isDisabled={true}>
-        Line XL Disabled
-      </Button>
-      <Button varient="line" size="large" isDisabled={false}>
-        Line L Enabled
-      </Button>
-      <Button varient="line" size="large" isDisabled={true}>
-        Line L Disabled
-      </Button>
-      <Button varient="line" size="medium" isDisabled={false}>
-        Line M Enabled
-      </Button>
-      <Button varient="line" size="medium" isDisabled={true}>
-        Line M Disabled
-      </Button>
-      <Button varient="line" size="small" isDisabled={false}>
-        Line S Enabled
-      </Button>
-      <Button varient="line" size="small" isDisabled={true}>
-        Line S Disabled
-      </Button>
-      <Button varient="line" size="xsmall" isDisabled={false}>
-        Line XS Enabled
-      </Button>
-      <Button varient="line" size="xsmall" isDisabled={true}>
-        Line XS Disabled
-      </Button>
-
-      {/* Gray Buttons */}
-      <h2>Gray</h2>
-      <Button varient="gray" size="xlarge" isDisabled={false}>
-        Gray XL Enabled
-      </Button>
-      <Button varient="gray" size="xlarge" isDisabled={true}>
-        Gray XL Disabled
-      </Button>
-      <Button varient="gray" size="large" isDisabled={false}>
-        Gray L Enabled
-      </Button>
-      <Button varient="gray" size="large" isDisabled={true}>
-        Gray L Disabled
-      </Button>
-      <Button varient="gray" size="medium" isDisabled={false}>
-        Gray M Enabled
-      </Button>
-      <Button varient="gray" size="medium" isDisabled={true}>
-        Gray M Disabled
-      </Button>
-      <Button varient="gray" size="small" isDisabled={false}>
-        Gray S Enabled
-      </Button>
-      <Button varient="gray" size="small" isDisabled={true}>
-        Gray S Disabled
-      </Button>
-      <Button varient="gray" size="xsmall" isDisabled={false}>
-        Gray XS Enabled
-      </Button>
-      <Button varient="gray" size="xsmall" isDisabled={true}>
-        Gray XS Disabled
-      </Button>
-
-      {/* Blue Buttons */}
-      <h2>Blue</h2>
-      <Button varient="blue" size="xlarge" isDisabled={false}>
-        Blue XL Enabled
+      <Button onClick={handleOnClick} varient="primary" size="xlarge" isDisabled={false}>
+        테스트2
       </Button>
 
       <RouterProvider router={router} />
--- src/components/commons/Button/Button.tsx
+++ src/components/commons/Button/Button.tsx
@@ -1,33 +1,50 @@
-import { ReactNode } from "react";
+import React, { ReactNode } from "react";
 import * as S from "./Button.styled";
 
 export interface ButtonPropTypes {
   size: "xlarge" | "large" | "medium" | "small" | "xsmall";
   isDisabled?: boolean;
   varient: "primary" | "line" | "gray" | "blue";
-  children: ReactNode;
+  children: ReactNode; //자식 노드(텍스트)를 받기 위해 필수.
+  onClick: React.MouseEventHandler<HTMLButtonElement> | undefined; //MouseEvent와는 상이
 }
 
-const Button = ({ size, isDisabled, varient, children }: ButtonPropTypes) => {
+const Button = ({ onClick, size, isDisabled, varient, children }: ButtonPropTypes) => {
   return (
     <>
       {varient === "primary" && (
-        <S.PrimaryButton size={size} isDisabled={isDisabled}>
+        <S.PrimaryButton
+          onClick={isDisabled ? undefined : onClick}
+          size={size}
+          isDisabled={isDisabled}
+        >
           {children}
         </S.PrimaryButton>
       )}
       {varient === "line" && (
-        <S.LineButton size={size} isDisabled={isDisabled}>
+        <S.LineButton
+          onClick={isDisabled ? undefined : onClick}
+          size={size}
+          isDisabled={isDisabled}
+        >
           {children}
         </S.LineButton>
       )}
       {varient === "gray" && (
-        <S.GrayButton size={size} isDisabled={isDisabled}>
+        <S.GrayButton
+          onClick={isDisabled ? undefined : onClick}
+          size={size}
+          isDisabled={isDisabled}
+        >
           {children}
         </S.GrayButton>
       )}
       {varient === "blue" && (
-        <S.BlueButton size={size} isDisabled={isDisabled}>
+        <S.BlueButton
+          onClick={isDisabled ? undefined : onClick}
+          size={size}
+          isDisabled={isDisabled}
+        >
           {children}
         </S.BlueButton>
       )}
