5055fe177cd3bab2cba5fc947d483867e16bf535
imddoy <imddoy@duksung.ac.kr>
2024-07-07 03:09:54 +0900

    feat: text clear 구현

--- src/components/commons/inputs/textFields/TextField.tsx
+++ src/components/commons/inputs/textFields/TextField.tsx
@@ -10,23 +10,35 @@ export interface TextFieldProps extends InputHTMLAttributes<HTMLInputElement> {
 
 const TextField = ({ maxLength, placeholder, narrow, unit }: TextFieldProps) => {
   const label = unit === "time" ? "분" : unit === "ticket" ? "매" : "초";
+  const [inputValue, setInputValue] = useState("");
   const [inputCount, setInputCount] = useState(0);
+
+  // 값 입력될 떄
   const onInputHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setInputValue(value);
     setInputCount(e.target.value.length);
+  };
+
+  // 값 지울 때
+  const clearInput = () => {
+    setInputValue("");
+    setInputCount(0);
   };
 
   return (
     <S.TextFieldLayout narrow={narrow}>
       <S.TextFieldWrapper>
         <S.TextFieldInput
+          value={inputValue}
           onChange={onInputHandler}
           maxLength={maxLength}
           placeholder={placeholder}
         ></S.TextFieldInput>
-        <S.TextClear />
+        {inputValue && <S.TextClear onClick={clearInput} />}
       </S.TextFieldWrapper>
       {unit && <S.TextUnit>{label}</S.TextUnit>}
-      {maxLength && <S.TextCap>{`${inputCount} / ${maxLength}`}</S.TextCap>}
+      {maxLength && <S.TextCap>{`${inputCount}/${maxLength}`}</S.TextCap>}
     </S.TextFieldLayout>
   );
 };
--- src/pages/test/TestPage.tsx
+++ src/pages/test/TestPage.tsx
@@ -6,7 +6,7 @@ const TestPage = () => {
     <div>
       <h1>Test Page</h1>
       <p>Test Page</p>
-      <TextArea placeholder="placeholder" maxLength={300} />
+      <TextField placeholder="placeholder" maxLength={300} />
     </div>
   );
 };
