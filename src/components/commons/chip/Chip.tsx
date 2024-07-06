73e670e6af748c4baa42117b29221360dd2d420e
jerry <becomegoodguy@gmail.com>
2024-07-06 23:04:30 +0900

    style: 타입명 변경

--- src/components/commons/chip/Chip.tsx
+++ src/components/commons/chip/Chip.tsx
@@ -3,14 +3,14 @@ import * as S from "./Chip.styled";
 
 export type ChipsColorTypes = "pink" | "white" | "gray";
 
-interface ChipsProps {
+interface ChipProps {
   label: string;
   color?: ChipsColorTypes;
   icon?: React.ReactNode;
   onClick?: () => void;
 }
 
-const Chip = ({ label, color, icon, onClick }: ChipsProps) => {
+const Chip = ({ label, color, icon, onClick }: ChipProps) => {
   return (
     <S.ChipWrapper color={color} onClick={onClick}>
       {icon && <S.ChipIcon>{icon}</S.ChipIcon>}
