4ac35b6f914403995d934a9251cacb7701423bb6
jerry <becomegoodguy@gmail.com>
2024-07-06 18:34:38 +0900

    feat: Chip 테스트 페이지에 추가

--- src/pages/test/TestPage.tsx
+++ src/pages/test/TestPage.tsx
@@ -1,8 +1,15 @@
+import { IconTextfiedlDelete } from "@assets/svgs";
+import Chip from "@components/commons/chip/Chip";
+
 const TestPage = () => {
   return (
     <div>
       <h1>Test Page</h1>
       <p>Test Page</p>
+      <div>
+        <Chip label="바보" color="pink" icon={<IconTextfiedlDelete />} />
+        <Chip label="테스트" />
+      </div>
     </div>
   );
 };
