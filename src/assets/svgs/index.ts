f549d450baa11b7320af5e134a24ec9548cc004d
jerry <becomegoodguy@gmail.com>
2024-07-07 18:19:46 +0900

    feat: 로고 svg 추가

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -3,3 +3,4 @@ export { default as IconArrowLeft } from "./IconArrowLeft";
 export { default as IconArrowRight } from "./IconArrowRight";
 export { default as IconTextfiedlDelete } from "./IconTextfiedlDelete";
 export { default as IconXButton } from "./IconXButton";
+export { default as Logo } from "./Logo";
