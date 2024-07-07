d5994ae6839909b8bde00d3ba935ae5ed6854619
jerry <becomegoodguy@gmail.com>
2024-07-08 02:17:15 +0900

    design: X 버튼 추가

--- src/assets/svgs/IconXButton.tsx
+++ src/assets/svgs/IconXButton.tsx
@@ -1,7 +1,13 @@
+import * as React from "react";
 import type { SVGProps } from "react";
 const SvgIconXButton = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 1548 691" {...props}>
-    <rect width={691} height={1548} x={1548} fill="#3E3E3E" rx={10} transform="rotate(90 1548 0)" />
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" {...props}>
+    <path
+      fill="#fff"
+      fillRule="evenodd"
+      d="M4.726 17a1 1 0 1 0 1.415 1.415l5.43-5.43 5.43 5.43a1 1 0 0 0 1.414-1.414l-5.43-5.43 5.43-5.43A1 1 0 0 0 17 4.726l-5.43 5.43-5.43-5.43a1 1 0 1 0-1.415 1.415l5.43 5.43z"
+      clipRule="evenodd"
+    />
   </svg>
 );
 export default SvgIconXButton;
