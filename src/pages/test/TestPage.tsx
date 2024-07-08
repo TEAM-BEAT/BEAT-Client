5fabfdb2d707dbd70ca04b93fcf8539d1d6c4d98
jerry <becomegoodguy@gmail.com>
2024-07-09 05:53:31 +0900

    feat: 테스트 페이지에 추가

--- src/pages/test/TestPage.tsx
+++ src/pages/test/TestPage.tsx
@@ -1,14 +1,18 @@
-import { IconTextfiedlDelete } from "@assets/svgs";
+import { IconCheck, IconTextfiedlDelete } from "@assets/svgs";
+import Button from "@components/commons/button/Button";
 import Chip from "@components/commons/chip/Chip";
 import TextArea from "@components/commons/input/textArea/TextArea";
 import TextField from "@components/commons/input/textField/TextField";
 import Stepper from "@components/commons/stepper/Stepper";
+import Toast from "@components/commons/toast/Toast";
+import useToast from "@hooks/useToast";
 import { useState } from "react";
 
 const TestPage = () => {
   const [inputValue, setInputValue] = useState("");
   const [inputAreaValue, setInputAreaValue] = useState("");
   const [round, setRound] = useState(1);
+  const { showToast, isToastVisible } = useToast();
 
   const handleChangeInput = (value: string) => {
     setInputValue(value);
@@ -48,6 +52,13 @@ const TestPage = () => {
         <Chip label="테스트" />
       </div>
       <Stepper max={3} round={round} onMinusClick={onMinusClick} onPlusClick={onPlusClick} />
+
+      <Button size="medium" variant="primary" onClick={showToast}>
+        토스트 보이기
+      </Button>
+      <Toast icon={<IconCheck />} isVisible={isToastVisible} toastBottom={3}>
+        클립보드에 복사되었습니다!
+      </Toast>
     </div>
   );
 };
