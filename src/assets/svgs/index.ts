a8a0e0ab942de3221f973cf3216fe2bf1a134a47
jerry <becomegoodguy@gmail.com>
2024-07-09 03:06:45 +0900

    design: checkIcon 추가

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,3 +1,4 @@
+export { default as IconCheck } from "./IconCheck";
 export { default as IconMinus } from "./IconMinus";
 export { default as IconPlus } from "./IconPlus";
 export { default as IconTextfiedlDelete } from "./IconTextfiedlDelete";
