776f876192b4538a4022e2a653ed4023a5463226
jerry <becomegoodguy@gmail.com>
2024-07-10 04:43:24 +0900

    feat: 고정 FooterButton 추가

--- src/pages/book/Book.tsx
+++ src/pages/book/Book.tsx
@@ -1,5 +1,7 @@
+import Button from "@components/commons/button/Button";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
 import Content from "./components/content/Content";
 import ShowInfo from "./components/showInfo/ShowInfo";
 import { SHOW_DETAIL_INFO } from "./constants";
@@ -28,8 +30,20 @@ const Book = () => {
         castList={detail.castList}
         staffList={detail.staffList}
       />
+      <FooterContainer>
+        {/* TODO: 토큰 여부에 따라서 리다이렉트 */}
+        <Button onClick={() => console.log("예매하기 클릭")}>예매하기</Button>
+      </FooterContainer>
     </div>
   );
 };
 
 export default Book;
+
+const FooterContainer = styled.div`
+  position: fixed;
+  bottom: 0;
+  padding: 2.4rem;
+
+  background-color: ${({ theme }) => theme.colors.gray_900};
+`;
