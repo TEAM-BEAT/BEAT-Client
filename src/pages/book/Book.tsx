649c5cbb87f0353d8fb1d49015f0b56e62e6a417
jerry <becomegoodguy@gmail.com>
2024-07-09 23:12:19 +0900

    feat: 공연 상세 정보 컴포넌트 추가

--- src/pages/book/Book.tsx
+++ src/pages/book/Book.tsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
+import posterExImg from "src/pages/MyRegisterdShow/constants/silkagel.png";
+import ShowInfo from "./components/showInfo/ShowInfo";
+import { SHOW_DETAIL_INFO } from "./constants";
 
 const Book = () => {
-  const { performanceId } = useParams<{ id: string }>();
+  // TODO: performanceId로 상세 정보 조회(GET)
+  const { performanceId } = useParams<{ performanceId: string }>();
+  const [detail, setDetail] = useState(SHOW_DETAIL_INFO);
 
-  console.log(performanceId);
-
-  return <div>Book</div>;
+  return (
+    <div>
+      <ShowInfo
+        posterImage={posterExImg}
+        title={detail.performanceTitle}
+        price={detail.ticketPrice}
+        venue={detail.performanceVenue}
+        period={detail.performancePeriod}
+        runningTime={detail.runningTime}
+        scheduleList={detail.scheduleList}
+      />
+    </div>
+  );
 };
 
 export default Book;
