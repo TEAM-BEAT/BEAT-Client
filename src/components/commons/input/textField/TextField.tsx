2b1e769c991d0cd8a317057f1a99da8b38792c50
jerry <becomegoodguy@gmail.com>
2024-07-10 18:56:36 +0900

    refactor: TextField 컴포넌트 개선

--- src/components/commons/input/textField/TextField.tsx
+++ src/components/commons/input/textField/TextField.tsx
@@ -1,60 +1,89 @@
-import { ChangeEvent, InputHTMLAttributes } from "react";
+import React, { ChangeEvent, InputHTMLAttributes, useRef } from "react";
 import * as S from "./TextField.styled";
 
 export interface TextFieldProps extends InputHTMLAttributes<HTMLInputElement> {
-  onChangeValue: (value: string) => void;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   maxLength?: number;
   placeholder: string;
   narrow?: false | true;
   unit?: "time" | "ticket" | "amount"; // 단위 : "분", "매", "원"
   filter?: (value: string) => string;
-  cap?: false | true;
 }
 
 const TextField = ({
+  name,
   value,
-  onChangeValue,
+  onChange,
   maxLength,
   placeholder,
   narrow,
   unit,
   filter,
-  cap,
   ...rest
 }: TextFieldProps) => {
   const label = unit === "time" ? "분" : unit === "ticket" ? "매" : "원";
 
+  const inputRef = useRef<HTMLInputElement>(null);
+
   // 값 입력될 떄
   const handleOnInput = (e: ChangeEvent<HTMLInputElement>) => {
+    const inputName = e.target.name;
     let inputValue = e.target.value;
+
     if (filter) {
       inputValue = filter(inputValue);
     }
     if (maxLength && inputValue.length > maxLength) {
       inputValue = inputValue.slice(0, maxLength);
     }
-    onChangeValue(inputValue);
+
+    const newEvent = {
+      ...e,
+      target: {
+        ...e.target,
+        name: inputName,
+        value: inputValue,
+      },
+    } as ChangeEvent<HTMLInputElement>;
+
+    onChange(newEvent);
   };
 
   // 값 지울 때
   const handleClearInput = () => {
-    onChangeValue("");
+    if (inputRef.current) {
+      const inputName = inputRef.current.name;
+
+      // 값 지우기
+      inputRef.current.value = "";
+
+      const newEvent = {
+        target: {
+          name: inputName,
+          value: "",
+        },
+      } as ChangeEvent<HTMLInputElement>;
+
+      onChange(newEvent);
+    }
   };
 
   return (
     <S.TextFieldLayout narrow={narrow}>
       <S.TextFieldWrapper>
         <S.TextFieldInput
-          {...rest}
+          ref={inputRef}
+          name={name}
           value={value}
           onChange={handleOnInput}
           maxLength={maxLength}
           placeholder={placeholder}
+          {...rest}
         />
         {!narrow && !unit && value && <S.TextClear onClick={handleClearInput} />}
         {unit && <S.TextUnit>{label}</S.TextUnit>}
       </S.TextFieldWrapper>
-      {maxLength && cap && <S.TextCap>{`${(value as string).length}/${maxLength}`}</S.TextCap>}
+      {maxLength && <S.TextCap>{`${(value as string).length}/${maxLength}`}</S.TextCap>}
     </S.TextFieldLayout>
   );
 };
