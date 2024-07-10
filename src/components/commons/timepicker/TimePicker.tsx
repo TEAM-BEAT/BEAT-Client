4a93b6a75e7b344be4a9fabdc0bf468d4af59c66
imddoy <imddoy@duksung.ac.kr>
2024-07-10 22:15:19 +0900

    chore: 필요없는 태그 삭제

--- src/components/commons/timepicker/TimePicker.tsx
+++ src/components/commons/timepicker/TimePicker.tsx
@@ -22,86 +22,84 @@ const TimePicker = ({ value, onChangeValue }: TimePickerProps) => {
     }
   };
   return (
-    <>
-      <S.CustomPicker
-        disablePast
-        showDaysOutsideCurrentMonth
-        format={"YYYY/MM/DD HH:mm"}
-        slots={{
-          day: StyledDay,
-          openPickerIcon: (props) => <S.CustomOpenPicker open={open} />,
-        }}
-        value={value}
-        onAccept={handleAccept}
-        slotProps={{
-          popper: {
-            sx: {
-              "& .MuiPaper-root": {
-                width: "32.7rem",
-                height: "33rem",
-              },
-              "& .MuiPickersLayout-root": {
-                maxWidth: "100%",
-                maxHeight: "100%",
-              },
-              "& .MuiPickersLayout-contentWrapper": {
-                width: "100%",
-                height: "28.7rem",
-              },
-              "& .MuiDateCalendar-root": {
-                width: "21rem",
-                height: "28.7rem",
-              },
-              "& .MuiTypography-caption": {
-                width: " 30px",
-                height: "31px",
-              },
-              "& .MuiPickersCalendarHeader-label": {
-                fontSize: "1.3rem",
-              },
-              "button.MuiPickersDay-root.Mui-selected": {
-                background: "#FB247F",
-              },
+    <S.CustomPicker
+      disablePast
+      showDaysOutsideCurrentMonth
+      format={"YYYY/MM/DD HH:mm"}
+      slots={{
+        day: StyledDay,
+        openPickerIcon: (props) => <S.CustomOpenPicker open={open} />,
+      }}
+      value={value}
+      onAccept={handleAccept}
+      slotProps={{
+        popper: {
+          sx: {
+            "& .MuiPaper-root": {
+              width: "32.7rem",
+              height: "33rem",
+            },
+            "& .MuiPickersLayout-root": {
+              maxWidth: "100%",
+              maxHeight: "100%",
+            },
+            "& .MuiPickersLayout-contentWrapper": {
+              width: "100%",
+              height: "28.7rem",
+            },
+            "& .MuiDateCalendar-root": {
+              width: "21rem",
+              height: "28.7rem",
+            },
+            "& .MuiTypography-caption": {
+              width: " 30px",
+              height: "31px",
+            },
+            "& .MuiPickersCalendarHeader-label": {
+              fontSize: "1.3rem",
+            },
+            "button.MuiPickersDay-root.Mui-selected": {
+              background: "#FB247F",
+            },
 
-              "& .MuiMultiSectionDigitalClockSection-item": {
-                width: "30px",
-                height: "20px",
-                fontSize: "1.3rem",
-              },
-              "& .MuiMultiSectionDigitalClockSection-item.Mui-selected": {
-                background: "#FB247F",
-              },
-              "&. MuiMultiSectionDigitalClock-root": {
-                height: "25.7rem",
-              },
+            "& .MuiMultiSectionDigitalClockSection-item": {
+              width: "30px",
+              height: "20px",
+              fontSize: "1.3rem",
+            },
+            "& .MuiMultiSectionDigitalClockSection-item.Mui-selected": {
+              background: "#FB247F",
+            },
+            "&. MuiMultiSectionDigitalClock-root": {
+              height: "25.7rem",
+            },
 
-              "& .MuiPickersDay-root": {
-                fontSize: "1.3rem",
-              },
-              "& .MuiDayCalendar-weekDayLabel": {
-                fontSize: "1.3rem",
-              },
-              "& .MuiButton-text": {
-                color: "#FB247F",
-                fontSize: "1.3rem",
-                margin: "0",
-                padding: "0",
-              },
-              "& .MuiPickersLayout-actionBar": {
-                display: "flex",
-                alignItems: "center",
-                height: "fit-content",
-              },
-              "& .MuiYearCalendar-root": {
-                width: "21rem",
-              },
+            "& .MuiPickersDay-root": {
+              fontSize: "1.3rem",
+            },
+            "& .MuiDayCalendar-weekDayLabel": {
+              fontSize: "1.3rem",
+            },
+            "& .MuiButton-text": {
+              color: "#FB247F",
+              fontSize: "1.3rem",
+              margin: "0",
+              padding: "0",
+            },
+            "& .MuiPickersLayout-actionBar": {
+              display: "flex",
+              alignItems: "center",
+              height: "fit-content",
+            },
+            "& .MuiYearCalendar-root": {
+              width: "21rem",
             },
           },
-        }}
-        onOpen={() => setOpen(true)}
-        onClose={() => setOpen(false)}
-      />
-    </>
+        },
+      }}
+      onOpen={() => setOpen(true)}
+      onClose={() => setOpen(false)}
+    />
   );
 };
 
