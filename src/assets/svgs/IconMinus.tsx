23ef6f8df25d563e3b528723623c985a61a1cd11
jerry <becomegoodguy@gmail.com>
2024-07-11 02:28:39 +0900

    remove: svg 파일 삭제

--- src/assets/svgs/IconMinus.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import type { SVGProps } from "react";
-const SvgIconMinus = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" {...props}>
-    <path stroke="#fff" strokeMiterlimit={10} strokeWidth={1.2} d="M17 12H7" />
-  </svg>
-);
-export default SvgIconMinus;
