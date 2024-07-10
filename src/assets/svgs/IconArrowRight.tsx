3af897bf41426f89cc36de1f041e8e485700cf1d
jerry <becomegoodguy@gmail.com>
2024-07-11 02:32:24 +0900

    style: 아이콘 추가

---
+++ src/assets/svgs/IconArrowRight.tsx
@@ -0,0 +1,12 @@
+import type { SVGProps } from "react";
+const SvgIconArrowRight = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" {...props}>
+    <path
+      fill="#fff"
+      fillRule="evenodd"
+      d="M7.83 19.546a1.125 1.125 0 0 1 0-1.591L13.784 12 7.83 6.045a1.125 1.125 0 1 1 1.59-1.59l6.75 6.75c.44.439.44 1.151 0 1.59l-6.75 6.75c-.439.44-1.151.44-1.59 0"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+export default SvgIconArrowRight;
---
+++ src/assets/svgs/IconCheck.tsx
@@ -0,0 +1,12 @@
+import type { SVGProps } from "react";
+const SvgIconCheck = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 16" {...props}>
+    <path
+      fill="#fff"
+      fillRule="evenodd"
+      d="M13.33 3.624a.5.5 0 0 1 .046.705l-7 8a.5.5 0 0 1-.73.025l-3-3a.5.5 0 1 1 .708-.708l2.622 2.622 6.648-7.597a.5.5 0 0 1 .705-.047"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+export default SvgIconCheck;
