c652ddae5278fe84a936574b36683284fc889f8a
Sinji <sinzi031012@naver.com>
2024-07-10 14:26:07 +0900

    feat: 계좌번호 모달 생성 (현재 children 안 됨 / 모달 바깥 선택시 오류)

--- src/pages/lookup/components/LookupWrapper.tsx
+++ src/pages/lookup/components/LookupWrapper.tsx
@@ -16,15 +16,17 @@ const LookupWrapper = ({ ...item }) => {
   };
 
   const handleAlert = () => {
+    // 이 부분 도영 오빠가 children 받을 수 있는 빈 모달 만들어주면 복사 가능하도록 변경
     openAlert({
       title: `${item.accountNumber}`,
-      subTitle: "얼럿입니다.",
       okCallback: () => {
         setAccountModal(false);
       },
     });
   };
 
+  // 현재 모달 바깥을 선택했을 때 account가 false로 변하지 않아 새로 열리지 않는데,
+  // 디자인 측에서 모달 바깥 선택하지 못하게 한다는 의견이 있어 일단 두겠습니다!
   useEffect(() => {
     if (accountModal) {
       handleAlert();
