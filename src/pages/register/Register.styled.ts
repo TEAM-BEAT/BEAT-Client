efcb05aca4902aff1ca56fb5f83792eee8c96a88
imddoy <imddoy@duksung.ac.kr>
2024-07-11 07:52:11 +0900

    feat: 등록 완료 페이지 틀 완성

---
+++ src/pages/register/Register.styled.ts
@@ -0,0 +1,35 @@
+import { Generators } from "@styles/generator";
+import styled from "styled-components";
+
+export const RegisterCompleteLayout = styled.section`
+  width: 37.4rem;
+  height: 60.8rem;
+  padding: 2.4rem;
+`;
+
+export const RegisterCompleteWrapper = styled.article`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 1.2rem 2.4rem 2.2rem;
+`;
+
+export const RegisterCompleteTitle = styled.h1`
+  color: ${({ theme }) => theme.colors.white};
+  ${({ theme }) => theme.fonts.heading3}
+  text-align: center;
+`;
+
+export const ReigsterCompleteSubTitle = styled.p`
+  color: ${({ theme }) => theme.colors.gray_300};
+  ${({ theme }) => theme.fonts["body2-normal-medi"]}
+  text-align: center;
+`;
+
+export const FooterContainer = styled.div`
+  position: sticky;
+  bottom: 0;
+  padding: 2.4rem;
+
+  background-color: ${({ theme }) => theme.colors.gray_900};
+`;
--- src/pages/register/RegisterComplete.tsx
+++ src/pages/register/RegisterComplete.tsx
@@ -1,10 +1,39 @@
 import Lottie from "react-lottie-player";
 import registerComplete from "@assets/lottie/register-complete.json";
+import * as S from "./Register.styled";
+import Spacing from "@components/commons/spacing/Spacing";
+import Button from "@components/commons/button/Button";
+import { useNavigate } from "react-router-dom";
 
 const RegisterComplete = () => {
+  const user = "메이커";
+  const navigate = useNavigate();
+  const performanceId = 1;
+  const goGigsPage = () => navigate(`/gig/${performanceId}`);
+
   return (
     <>
-      <Lottie loop animationData={registerComplete} play={true} />
+      <S.RegisterCompleteLayout>
+        <S.RegisterCompleteWrapper>
+          <Lottie
+            animationData={registerComplete}
+            play={true}
+            style={{ width: "150px", height: "150px", margin: "0 auto" }}
+          />
+          <Spacing marginBottom="3.2" />
+          <S.RegisterCompleteTitle>
+            {user}님, <br />
+            공연 등록이 완료되었어요!
+          </S.RegisterCompleteTitle>
+          <S.ReigsterCompleteSubTitle>
+            가슴 뛰는 이야기의 시작을 축하드려요.
+          </S.ReigsterCompleteSubTitle>
+          <Spacing marginBottom="1" />
+        </S.RegisterCompleteWrapper>
+      </S.RegisterCompleteLayout>
+      <S.FooterContainer>
+        <Button onClick={goGigsPage}>방금 등록한 공연 보러가기 </Button>
+      </S.FooterContainer>
     </>
   );
 };
