91e1949e2ef2b7e872d51bfe810682b8f6a90977
imddoy <imddoy@duksung.ac.kr>
2024-07-10 21:44:50 +0900

    refactor: mui 다크모드 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
 import Alert from "@components/commons/modal/Alert";
 import Confirm from "@components/commons/modal/Confirm";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import router from "@routes/Router";
 import GlobalStyle from "@styles/global";
 import theme from "@styles/theme";
 import { RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider as MuiThemeProvider, createTheme } from "@mui/material/styles";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <GlobalStyle />
-        <RouterProvider router={router} />
-        <Alert />
-        <Confirm />
-      </LocalizationProvider>
-    </ThemeProvider>
+    <MuiThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <GlobalStyle />
+          <RouterProvider router={router} />
+          <Alert />
+          <Confirm />
+        </LocalizationProvider>
+      </ThemeProvider>
+    </MuiThemeProvider>
   );
 }
 
