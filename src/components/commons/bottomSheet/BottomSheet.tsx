666b1b0544a486fb2ec306b9ef2cbf73ae83380f
jerry <becomegoodguy@gmail.com>
2024-07-11 18:37:05 +0900

    feat: BottomSheet 컴포넌트에 isOpen props 및 애니메이션 추가

--- src/components/commons/bottomSheet/BottomSheet.tsx
+++ src/components/commons/bottomSheet/BottomSheet.tsx
@@ -2,13 +2,14 @@ import { ReactNode } from "react";
 import * as S from "./BottomSheet.styled";
 
 export interface BottomSheetPropType {
+  isOpen: boolean;
   children?: ReactNode;
   title?: string;
 }
 
-const BottomSheet = ({ title, children }: BottomSheetPropType) => {
+const BottomSheet = ({ isOpen, title, children }: BottomSheetPropType) => {
   return (
-    <S.BottomSheetWrapper onClick={(e) => e.stopPropagation()}>
+    <S.BottomSheetWrapper isOpen={isOpen} onClick={(e) => e.stopPropagation()}>
       <S.BottomSheetLayout>
         <S.Title>{title}</S.Title>
         {children}
