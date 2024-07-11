0616c02cb6dabcd6fee42881f51981d67243c660
jerry <becomegoodguy@gmail.com>
2024-07-11 16:33:08 +0900

    feat: 예매자 정보 props로 값 변경할 수 있도록 변경

--- src/pages/book/components/bookerInfo/BookerInfo.tsx
+++ src/pages/book/components/bookerInfo/BookerInfo.tsx
@@ -4,16 +4,27 @@ import React from "react";
 import * as S from "./BookerInfo.styled";
 
 interface BookerInfoProps {
+  bookerInfo: {
+    name: string;
+    phoneNumber: string;
+  };
   onChangeBookerInfo: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const BookerInfo = ({ onChangeBookerInfo }: BookerInfoProps) => {
+const BookerInfo = ({ bookerInfo, onChangeBookerInfo }: BookerInfoProps) => {
   return (
     <S.Wrapper>
       <S.Title>예매자 정보</S.Title>
-      <TextField name="name" onChange={onChangeBookerInfo} placeholder="예매자(대표 예매자) 이름" />
+      <TextField
+        name="name"
+        value={bookerInfo.name}
+        onChange={onChangeBookerInfo}
+        placeholder="예매자(대표 예매자) 이름"
+      />
       <TextField
         name="phoneNumber"
+        type="input"
+        value={bookerInfo.phoneNumber}
         onChange={onChangeBookerInfo}
         filter={phoneNumberFilter}
         placeholder="휴대폰 번호 (‘-’ 없이 입력)"
