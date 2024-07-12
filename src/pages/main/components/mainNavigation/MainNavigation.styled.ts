cc10eab7a668f8a9b1fcead281a7e1f082d35a53
Sinji <sinzi031012@naver.com>
2024-07-13 04:45:46 +0900

    feat: 레이아웃만 추가

---
+++ src/pages/main/components/mainNavigation/MainNavigation.styled.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const MainNavigationWrapper = styled.section`
+  position: absolute;
+  z-index: 1;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 37.5rem;
+  height: 5.6rem;
+
+  color: white;
+`;
---
+++ src/pages/main/components/mainNavigation/MainNavigation.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import * as S from "./MainNavigation.styled";
+
+const MainNavigation = () => {
+  return <S.MainNavigationWrapper>테스트테스트테스트</S.MainNavigationWrapper>;
+};
+
+export default MainNavigation;
