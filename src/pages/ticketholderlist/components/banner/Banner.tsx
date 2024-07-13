da570d5c3592347d81df8677b5df96e9aca2b846
ocahs9 <piorag99@gmail.com>
2024-07-13 22:46:07 +0900

    feat: BannerTitle ellipsis 설정

--- src/pages/ticketholderlist/components/banner/Banner.tsx
+++ src/pages/ticketholderlist/components/banner/Banner.tsx
@@ -12,7 +12,7 @@ const Banner = ({ image, reservedCount, isOutdated }: BannerProps) => {
       <S.BannerTextLayout>
         <S.BannerTextBox>
           <S.BannerTextBox>
-            <S.BannerTitleText>실리카겔 락앤롤</S.BannerTitleText>
+            <S.BannerTitleText>실리카겔 락앤롤롤롤롤롤롤롤롤롤롤롤</S.BannerTitleText>
             {isOutdated ? (
               <S.BannerStateTextBox>
                 총<S.CountTextSpan> {reservedCount}매 </S.CountTextSpan>
@@ -20,7 +20,7 @@ const Banner = ({ image, reservedCount, isOutdated }: BannerProps) => {
               </S.BannerStateTextBox>
             ) : (
               <S.BannerStateTextBox>
-                총<S.CountTextSpan> {reservedCount}매 </S.CountTextSpan>
+                <S.CountTextSpan> {reservedCount}매 </S.CountTextSpan>
                 예매됨
               </S.BannerStateTextBox>
             )}
