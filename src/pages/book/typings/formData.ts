1470b6ba0de232272c48285ffb1fb10d33b3f292
jerry <becomegoodguy@gmail.com>
2024-07-14 22:49:46 +0900

    fix: formData 타입 변경

--- src/pages/book/typings/formData.ts
+++ src/pages/book/typings/formData.ts
@@ -3,8 +3,8 @@ export interface FormData {
   selectedValue: number | undefined;
   purchaseTicketCount: number;
   totalPaymentAmount: number;
-  bookerName?: string;
-  bookerPhoneNumber?: string;
+  bookerName: string;
+  bookerPhoneNumber: string;
   // 비회원일 경우에만 추가될 속성
   birthDate?: string;
   password?: string;
