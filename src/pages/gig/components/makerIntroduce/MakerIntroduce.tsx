c4f4a3e141fa5eb25b2fc6f907c755941b5f1022
DOYEONG <58854041+pepperdad@users.noreply.github.com>
2024-07-15 19:45:38 +0900

    Update MakerIntroduce.tsx

--- src/pages/gig/components/makerIntroduce/MakerIntroduce.tsx
+++ src/pages/gig/components/makerIntroduce/MakerIntroduce.tsx
@@ -45,7 +45,7 @@ const MakerIntroduce = ({ teamName, castList, staffList }: MakerIntroduceProps) 
             ))}
           </S.TeamPeopleCardWrapper>
         ) : (
-          <S.NoContentBox>등록된 메이커 정보가 없어요.</S.NoContentBox>
+          <S.NoContentBox>등록된 스태프 정보가 없어요.</S.NoContentBox>
         )}
       </S.MakerInfoContainer>
     </S.Wrapper>
