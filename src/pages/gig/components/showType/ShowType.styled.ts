9b1a9bd1f1b362a9ca9089a4162b2a705593bf00
jerry <becomegoodguy@gmail.com>
2024-07-15 17:40:07 +0900

    feat: ShowType 컴포넌트 아이콘과 함께 보여주도록 수정

--- src/pages/gig/components/showType/ShowType.styled.ts
+++ src/pages/gig/components/showType/ShowType.styled.ts
@@ -10,12 +10,13 @@ export const ShowTypeWrapper = styled.div`
 export const ShowTypeIcon = styled.span`
   width: 1.6rem;
   height: 1.6rem;
-  margin-right: 0.4rem;
 
-  background-color: ${({ theme }) => theme.colors.gray_500};
+  color: ${({ theme }) => theme.colors.gray_500};
 `;
 
 export const ShowTypeText = styled.span`
+  margin-left: 0.4rem;
+
   ${({ theme }) => theme.fonts["caption2-semi"]};
   color: ${({ theme }) => theme.colors.gray_500};
 `;
