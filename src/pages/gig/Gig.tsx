8c696676a43e0df38e7e6b032739f6cbf9af4648
jerry <becomegoodguy@gmail.com>
2024-07-15 17:02:00 +0900

    feat: 헤더 추가 및 로그인 바텀시트 추가

--- src/pages/gig/Gig.tsx
+++ src/pages/gig/Gig.tsx
@@ -1,5 +1,9 @@
+import ActionBottomSheet from "@components/commons/bottomSheet/actionsBottomSheet/ActionBottomSheet";
+import OuterLayout from "@components/commons/bottomSheet/OuterLayout";
 import Button from "@components/commons/button/Button";
-import { useState } from "react";
+import { NAVIGATION_STATE } from "@constants/navigationState";
+import { useHeader } from "@hooks/useHeader";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import styled from "styled-components";
 import Content from "./components/content/Content";
@@ -8,12 +12,33 @@ import { SHOW_DETAIL_INFO } from "./constants";
 
 const Gig = () => {
   const navigate = useNavigate();
+  const { setHeader } = useHeader();
+
+  useEffect(() => {
+    setHeader({
+      headerStyle: NAVIGATION_STATE.ICON_TITLE_SUB_TEXT,
+      title: detail.performanceTitle,
+      leftOnClick: () => {
+        navigate("/main");
+      },
+    });
+  }, []);
+
   // TODO: performanceId로 상세 정보 조회(GET)
   const { performanceId } = useParams<{ performanceId: string }>();
   const [detail, setDetail] = useState(SHOW_DETAIL_INFO);
+  const [isSheetOpen, setIsSheetOpen] = useState(false);
+
+  const handleBookClick = () => {
+    setIsSheetOpen(true);
+  };
+
+  const handleSheetClose = () => {
+    setIsSheetOpen(false);
+  };
 
   return (
-    <div>
+    <ContentWrapper>
       <ShowInfo
         posterImage={detail.posterImage}
         title={detail.performanceTitle}
@@ -33,13 +58,37 @@ const Gig = () => {
       />
       <FooterContainer>
         {/* TODO: 토큰 여부에 따라서 리다이렉트 */}
-        <Button onClick={() => navigate("/book/1")}>예매하기</Button>
+        <Button onClick={handleBookClick}>예매하기</Button>
       </FooterContainer>
-    </div>
+
+      <ActionBottomSheet
+        isOpen={isSheetOpen}
+        onClickOutside={handleSheetClose}
+        title="로그인 후 예매하시겠어요?"
+        alignItems="center"
+        padding="2rem 2rem 2.4rem 2rem"
+      >
+        <OuterLayout margin="1.6rem 0 0 0">
+          <Button variant="primary" size="xlarge">
+            확인했어요
+          </Button>
+          <Button variant="primary" size="xlarge">
+            확인했어요
+          </Button>
+        </OuterLayout>
+      </ActionBottomSheet>
+    </ContentWrapper>
   );
 };
 
 export default Gig;
+
+export const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 2.4rem;
+`;
 
 const FooterContainer = styled.div`
   position: sticky;
