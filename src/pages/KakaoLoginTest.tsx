0e837cb081572e6314c327479e3d1bcd191591ae
Sinji <sinzi031012@naver.com>
2024-07-16 05:24:43 +0900

    fix: 카카오로그인 시 원하는 flow로 이동할 수 있도록 수정

--- src/pages/KakaoLoginTest.tsx
+++ src/pages/KakaoLoginTest.tsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { useAtom } from "jotai";
+import { navigateAtom } from "@stores/navigate";
 
 import Button from "@components/commons/button/Button";
 
@@ -7,9 +9,17 @@ import { requestKakaoLogin } from "@utils/kakaoLogin";
 // 실제 사용할 때는 원하는 페이지에 button onClick에 requestKakaoLogin만 주면 됨
 
 const KakaoLoginTest = () => {
+  const [, setNavigateUrl] = useAtom(navigateAtom);
+
+  const handleKakaoLogin = (url: string) => {
+    setNavigateUrl(url);
+    console.log(url);
+    requestKakaoLogin();
+  };
+
   return (
     <Test>
-      <Button onClick={requestKakaoLogin}>카카오 로그인 테스트~~</Button>
+      <Button onClick={() => handleKakaoLogin("/gig-register")}>카카오 로그인 테스트~~</Button>
     </Test>
   );
 };
