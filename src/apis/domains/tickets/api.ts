aa5ccdbd6a3a60ddb8389b806e5ceb543d533e60
ocahs9 <piorag99@gmail.com>
2024-07-17 05:41:28 +0900

    feat: 예매자 입릅 여부 수정 API 및 쿼리 작성

--- src/apis/domains/tickets/api.ts
+++ src/apis/domains/tickets/api.ts
@@ -40,19 +40,21 @@ export interface putTicketReq {
 type TicketUpdateRequest = components["schemas"]["TicketUpdateRequest"];
 type SuccessResponseVoid = components["schemas"]["SuccessResponseVoid"];
 
-
 //async 함수는 항상 promise로 감싸서 값을 리턴한다.
 //즉, 비동기 함수의 값을 사용하려면 추후 await이나 then 을 사용해야 한다던데..
 //이해가 잘 안가는걸 보니 아직 프로미스에 대한 개념 부족이라 판단됨
 export const putTicketUpdate = async (
-  formData : TicketUpdateRequest
+  formData: TicketUpdateRequest
 ): Promise<SuccessResponseVoid | null> => {
   try {
     const response: AxiosResponse<ApiResponseType<SuccessResponseVoid>> = await put(
       `tickets/${formData.performanceId}`,
       formData
     );
-    
+
     return response.data.data;
+  } catch (error) {
+    console.log("error", error);
+    return null;
   }
-}
+};
--- src/apis/domains/tickets/queries.ts
+++ src/apis/domains/tickets/queries.ts
@@ -1,5 +1,5 @@
-import { useQuery } from "@tanstack/react-query";
-import { getTicketReq, getTicketRetrieve } from "./api";
+import { QueryClient, useMutation, useQuery } from "@tanstack/react-query";
+import { getTicketReq, getTicketRetrieve, TicketUpdateRequest } from "./api";
 
 // 예매자 목록 조회 API (GET)를 위한 쿼리 작성
 const QUERY_KEY = {
@@ -12,5 +12,17 @@ export const useTicketRetrive = (formData: getTicketReq) => {
     queryFn: () => getTicketRetrieve(formData),
     staleTime: 1000 * 60 * 60,
     gcTime: 1000 * 60 * 60 * 24,
+  });
+};
+
+// 예매자 입급 여부 수정 API (PUT)를 위한 쿼리 작성
+export const useTicketUpdate = () => {
+  const queryClient = new QueryClient();
+
+  return useMutation({
+    mutationFn: (formData: TicketUpdateRequest) => putTicketUpdate(formData),
+    onSuccess: (res) => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEY.LIST] });
+    },
   });
 };
