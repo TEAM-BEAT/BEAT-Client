0d60b34932718af11a7ff97a2711a7f247a20ab1
ocahs9 <piorag99@gmail.com>
2024-07-18 03:30:42 +0900

    feat: 공연 수정 페이지 정보 조회 쿼리 작성:

--- src/apis/domains/performances/queries.ts
+++ src/apis/domains/performances/queries.ts
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { getMakerPerformance } from "./api";
+import { getMakerPerformance, getPerformanceEdit } from "./api";
 
 const QUERY_KEY = {
   LIST: "list",
@@ -10,6 +10,16 @@ export const useMakerPerformance = () => {
   return useQuery({
     queryKey: [QUERY_KEY.LIST],
     queryFn: getMakerPerformance,
+    staleTime: 1000 * 60 * 60,
+    gcTime: 1000 * 60 * 60 * 24,
+  });
+};
+
+// 공연 수정 페이지 정보 조회 API (GET) 를 위한 쿼리 작성
+export const usePerformanceEdit = (performanceId: number) => {
+  return useQuery({
+    queryKey: [QUERY_KEY.LIST],
+    queryFn: () => getPerformanceEdit(performanceId),
     staleTime: 1000 * 60 * 60,
     gcTime: 1000 * 60 * 60 * 24,
   });
