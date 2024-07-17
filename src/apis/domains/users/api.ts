81ebd2ff7b923f7e60f41b1f41eccc19d66d8c7d
Sinji <sinzi031012@naver.com>
2024-07-18 04:32:07 +0900

    fix: 로그인 시 로컬스토리지에 사용자 정보 안 남는 오류 수정

--- src/apis/domains/users/api.ts
+++ src/apis/domains/users/api.ts
@@ -1,18 +1,13 @@
 import { post } from "@apis/index";
+import { components } from "@typings/api/schema";
 import { ApiResponseType } from "@typings/commonType";
 import { AxiosResponse } from "axios";
 
-type LoginSuccessReponse = {
-  data: {
-    accessToken: string;
-    refreshToken: string;
-    nickName: string;
-  };
-};
+type LoginSuccessResponse = components["schemas"]["LoginSuccessResponse"];
 
-export const postKakaoLogin = async (authCode: string): Promise<LoginSuccessReponse | null> => {
+export const postKakaoLogin = async (authCode: string): Promise<LoginSuccessResponse | null> => {
   try {
-    const response: AxiosResponse<ApiResponseType<LoginSuccessReponse>> = await post(
+    const response: AxiosResponse<ApiResponseType<LoginSuccessResponse>> = await post(
       `/users/sign-up?authorizationCode=${authCode}`,
       {
         socialType: "KAKAO",
--- src/apis/domains/users/queries.ts
+++ src/apis/domains/users/queries.ts
@@ -8,29 +8,28 @@ const QUERY_KEY = {
   KAKAO_LOGIN: "kakaoLogin",
 };
 
-type LoginSuccessReponse = {
-  data: {
-    accessToken: string;
-    refreshToken: string;
-    nickName: string;
-  };
-};
+type LoginSuccessResponse = components["schemas"]["LoginSuccessResponse"];
 
 export const usePostKakaoLogin = () => {
   const [, setUserData] = useAtom(userAtom);
   const queryClient = new QueryClient();
 
-  return useMutation<LoginSuccessReponse | null, unknown, string>({
+  return useMutation<LoginSuccessResponse | null, unknown, string>({
     mutationFn: (authCode: string) => postKakaoLogin(authCode),
     onSuccess: (response) => {
       if (response) {
         console.log("login success", response);
 
-        const userData = response.data;
+        const userData = response;
 
         if (userData) {
-          const { accessToken, refreshToken, nickName } = userData;
-          setUserData({ nickName, accessToken });
+          const { accessToken, nickname } = userData;
+
+          if (accessToken && nickname) {
+            setUserData({ nickname, accessToken });
+          } else {
+            console.error("accessToken or nickname is undefined");
+          }
         }
 
         queryClient.invalidateQueries({ queryKey: [QUERY_KEY.KAKAO_LOGIN] });
