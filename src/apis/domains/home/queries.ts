9f0407d0f0885748d6f190ad09dc2cb9483bdc92
Sinji <sinzi031012@naver.com>
2024-07-19 23:02:04 +0900

    fix: stale time 0으로 세팅

--- src/apis/domains/home/queries.ts
+++ src/apis/domains/home/queries.ts
@@ -10,7 +10,8 @@ export const useGetAllScheduleList = () => {
   return useQuery({
     queryKey: [HOME_QUERY_KEY.LIST],
     queryFn: () => getAllScheduleList(), // API 요청 함수
-    staleTime: 1000 * 60 * 60,
+    // staleTime: 1000 * 60 * 60,
+    staleTime: 0,
     gcTime: 1000 * 60 * 60 * 24,
   });
 };
