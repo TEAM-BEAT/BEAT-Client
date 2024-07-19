3742803eae42915a4316a01a4c4fc9075f9c464c
Sinji <sinzi031012@naver.com>
2024-07-19 19:05:13 +0900

    fix: ios 스태퍼 색 안 뜨는 오류 수정

--- src/assets/svgs/IconMinus.tsx
+++ src/assets/svgs/IconMinus.tsx
@@ -1,6 +1,6 @@
 import type { SVGProps } from "react";
 const SvgIconMinus = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" {...props}>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="#FFF" viewBox="0 0 24 24" {...props}>
     <path stroke="current" strokeMiterlimit={10} strokeWidth={1.2} d="M17 12H7" />
   </svg>
 );
