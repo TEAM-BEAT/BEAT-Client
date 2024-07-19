e207c8f9bfdc0268a1206618924036a8d2724052
ocahs9 <piorag99@gmail.com>
2024-07-19 19:07:34 +0900

    라디오 버튼 svg 변경

--- src/assets/svgs/IconCheckboxSelectedOn.tsx
+++ src/assets/svgs/IconCheckboxSelectedOn.tsx
@@ -1,15 +1,15 @@
 import type { SVGProps } from "react";
 const SvgIconCheckboxSelectedOn = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 19 18" {...props}>
-    <rect width={17} height={17} x={1} y={0.5} fill="#B80450" rx={1.5} />
-    <rect width={17} height={17} x={1} y={0.5} stroke="#B80450" rx={1.5} />
+  <svg xmlns="http://www.w3.org/2000/svg" width="19" height="18" viewBox="0 0 19 18" fill="none">
+    <rect x="1" y="0.5" width="17" height="17" rx="1.5" fill="#B80450" />
+    <rect x="1" y="0.5" width="17" height="17" rx="1.5" stroke="#FF97C2" />
     <path
-      fill="#fff"
-      stroke="#fff"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth={0.27}
-      d="M14.59 5.634a.585.585 0 0 0-.881-.77l-5.888 6.729-2.258-2.258a.585.585 0 1 0-.827.827l2.7 2.7a.585.585 0 0 0 .853-.028z"
+      d="M14.5895 5.63406C14.8022 5.39091 14.7776 5.02133 14.5344 4.80858C14.2913 4.59582 13.9217 4.62046 13.709 4.86361L7.82072 11.593L5.56288 9.33518C5.33442 9.10672 4.96402 9.10672 4.73556 9.33518C4.5071 9.56363 4.5071 9.93403 4.73556 10.1625L7.43556 12.8625C7.55005 12.977 7.70687 13.0389 7.86869 13.0335C8.03051 13.0281 8.18286 12.9559 8.28948 12.8341L14.5895 5.63406Z"
+      fill="white"
+      stroke="white"
+      stroke-width="0.27"
+      stroke-linecap="round"
+      stroke-linejoin="round"
     />
   </svg>
 );
