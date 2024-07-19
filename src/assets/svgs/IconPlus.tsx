accd27f3ccbd56229c67b7ac8345b5d355b9f5d0
Sinji <sinzi031012@naver.com>
2024-07-19 19:20:41 +0900

    fix: ios 스태퍼 색 안 뜨는 오류 수정

--- src/assets/svgs/IconPlus.tsx
+++ src/assets/svgs/IconPlus.tsx
@@ -1,6 +1,13 @@
 import type { SVGProps } from "react";
 const SvgIconPlus = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="#FFF" viewBox="0 0 24 24" {...props}>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="#FFF"
+    viewBox="0 0 24 24"
+    width="24"
+    height="24"
+    {...props}
+  >
     <path stroke="current" strokeMiterlimit={10} strokeWidth={1.2} d="M12 7v10M17 12H7" />
   </svg>
 );
