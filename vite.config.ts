16d22478ab1b8192d61c675369f3a18d1c7f5d39
jerry <becomegoodguy@gmail.com>
2024-08-08 01:17:56 +0900

    fix: remove Dockerfile

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import prerender from "@prerenderer/rollup-plugin";
+import chromium from "@sparticuz/chromium-min";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig, loadEnv } from "vite";
 import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { generatePerformanceRoutes } from "./src/utils/generatePerformanceRoute";
-import chromium from "@sparticuz/chromium-min";
 
 // https://vitejs.dev/config/
 export default defineConfig(async ({ mode }) => {
