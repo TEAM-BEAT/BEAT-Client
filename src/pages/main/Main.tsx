716ad043d8173bbc28a752bc79da3abb6c229985
jerry <becomegoodguy@gmail.com>
2024-08-24 20:11:40 +0900

    test: 메인 빌드 테스트

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,22 +1,13 @@
 import { useState } from "react";
-import * as S from "./Main.styled";
 
 import Loading from "@components/commons/loading/Loading";
 
-import { useGetAllScheduleList } from "@apis/domains/home/queries";
-import {
-  Carousel,
-  Chips,
-  Floating,
-  Footer,
-  MainNavigation,
-  Performance,
-} from "@pages/main/components";
+import { useGetPerformanceDetail } from "@apis/domains/performances/queries";
 import { navigateAtom } from "@stores";
 import { useAtom } from "jotai";
 
 const Main = () => {
-  const { data, isLoading } = useGetAllScheduleList();
+  const { data, isLoading } = useGetPerformanceDetail(116);
 
   const [genre, setGenre] = useState("ALL");
   const [navigateUrl, setNavigateUrl] = useAtom(navigateAtom);
@@ -63,22 +54,7 @@ const Main = () => {
     return <Loading />;
   }
 
-  return (
-    <S.MainWrapper>
-      {/* <button style={{ color: "white" }} onClick={onClickHi}>
-            하이 테스트
-          </button>
-          <button style={{ color: "white" }} onClick={onClickHello}>
-            헬로 테스트
-          </button> */}
-      <MainNavigation />
-      <Carousel promotionList={data?.promotionList ?? []} />
-      <Chips handleGenre={handleGenre} />
-      <Floating />
-      <Performance genre={genre} performanceList={data?.performanceList ?? []} />
-      <Footer />
-    </S.MainWrapper>
-  );
+  return "테스트";
 };
 
 export default Main;
