11088ab18cca1887c2a84ddc18c26d9b4c71e732
jerry <becomegoodguy@gmail.com>
2024-08-24 20:19:33 +0900

    test: 메인 페이지 빌드 테스트

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,60 +1,20 @@
-import { useState } from "react";
-
 import Loading from "@components/commons/loading/Loading";
 
 import { useGetPerformanceDetail } from "@apis/domains/performances/queries";
-import { navigateAtom } from "@stores";
-import { useAtom } from "jotai";
+import NotFound from "@pages/notFound/NotFound";
 
 const Main = () => {
   const { data, isLoading } = useGetPerformanceDetail(116);
-
-  const [genre, setGenre] = useState("ALL");
-  const [navigateUrl, setNavigateUrl] = useAtom(navigateAtom);
-
-  const handleGenre = (value: string) => {
-    setGenre(value);
-  };
-
-  const onClickHi = async () => {
-    const res = await fetch("/api/hi", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    console.log("testres is: ", res.json());
-
-    if (res.ok) {
-      console.log("testres successful");
-    } else {
-      console.error("testres failed");
-    }
-  };
-
-  const onClickHello = async () => {
-    const res = await fetch("/api/hello", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    console.log("testres is: ", res.json());
-
-    if (res.ok) {
-      console.log("testres successful");
-    } else {
-      console.error("testres failed");
-    }
-  };
 
   if (isLoading) {
     return <Loading />;
   }
 
-  return "테스트";
+  if (!data) {
+    return <NotFound />;
+  }
+
+  return <div>{data?.posterImage}</div>;
 };
 
 export default Main;
