2bc8ab9ca2add78b3a15eed91c8271f353d07cd4
jerry <becomegoodguy@gmail.com>
2024-08-24 13:47:46 +0900

    revert: 서버 로그 테스트 확인

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,6 +1,17 @@
 import { useState } from "react";
+import * as S from "./Main.styled";
+
+import Loading from "@components/commons/loading/Loading";
 
 import { useGetAllScheduleList } from "@apis/domains/home/queries";
+import {
+  Carousel,
+  Chips,
+  Floating,
+  Footer,
+  MainNavigation,
+  Performance,
+} from "@pages/main/components";
 import { navigateAtom } from "@stores";
 import { useAtom } from "jotai";
 
@@ -49,11 +60,28 @@ const Main = () => {
     }
   };
 
-  if (isLoading) {
-    return "로딩 중";
-  }
-
-  return "테스트";
+  return (
+    <>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <S.MainWrapper>
+          {/* <button style={{ color: "white" }} onClick={onClickHi}>
+            하이 테스트
+          </button>
+          <button style={{ color: "white" }} onClick={onClickHello}>
+            헬로 테스트
+          </button> */}
+          <MainNavigation />
+          <Carousel promotionList={data?.promotionList ?? []} />
+          <Chips handleGenre={handleGenre} />
+          <Floating />
+          <Performance genre={genre} performanceList={data?.performanceList ?? []} />
+          <Footer />
+        </S.MainWrapper>
+      )}
+    </>
+  );
 };
 
 export default Main;
