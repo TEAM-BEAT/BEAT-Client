43402d7d484542cf4ae0382703db5208e636d5dd
Sinji <sinzi031012@naver.com>
2024-08-26 22:59:17 +0900

    chore: 쿼리키 로직 제거

--- src/apis/kakaoLogin/queries.ts
+++ src/apis/kakaoLogin/queries.ts
@@ -1,19 +1,14 @@
-import { QueryClient, useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { postLogout } from "./api";
 
-export const LOGOUT_QUERY_KEY = { LOGOUT: "logout" };
-
 export const usePostLogout = () => {
-  const queryClient = new QueryClient();
+  const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: () => postLogout(),
     onSuccess: (res) => {
-      queryClient.invalidateQueries({ queryKey: [LOGOUT_QUERY_KEY.LOGOUT] });
-      queryClient.refetchQueries({
-        queryKey: [LOGOUT_QUERY_KEY.LOGOUT],
-        exact: true,
-      });
+      queryClient.invalidateQueries();
+      queryClient.refetchQueries();
     },
   });
 };
