c1049c269da739e537fecdb4b3d93274a59a53f7
jerry <becomegoodguy@gmail.com>
2024-08-29 22:17:36 +0900

    feat: 상세 이미지 보여주는 컴포넌트 추가

---
+++ src/pages/gig/components/detailImage/DetailImage.styled.ts
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ImageWrapper = styled.div<{ $showAllImages: boolean; $toggleAvailable: boolean }>`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  height: ${({ $showAllImages, $toggleAvailable }) =>
+    $showAllImages ? "auto" : $toggleAvailable && "700px"};
+  overflow: ${({ $showAllImages }) =>
+    $showAllImages ? "visible" : "hidden"}; /* 이미지의 하단 부분을 잘라내도록 설정 */
+`;
+
+export const Image = styled.img`
+  width: 100%;
+  height: auto;
+`;
+
+export const ShowMoreButton = styled.button`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 8px;
+  padding: 1.6rem 9.1rem;
+
+  color: ${({ theme }) => theme.colors.white};
+  ${({ theme }) => theme.fonts["body1-normal-semi"]};
+
+  background-color: ${({ theme }) => theme.colors.gray_900};
+  cursor: pointer;
+  border: 1px solid ${({ theme }) => theme.colors.gray_700};
+  border-radius: 6px;
+`;
+
+export const Overlay = styled.div`
+  position: absolute;
+  bottom: 6.3rem;
+
+  width: 100%;
+  height: 10rem;
+
+  background: linear-gradient(180deg, rgb(27 27 27 / 0%) 0%, #1b1b1b 100%);
+`;
