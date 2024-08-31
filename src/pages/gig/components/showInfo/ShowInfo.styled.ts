a1923ed0a9c25af66ee41a10c69a949bb13b2ec5
imddoy <imddoy@duksung.ac.kr>
2024-09-01 00:01:19 +0900

    feat: 이미지 원본 보여주기

--- src/pages/gig/components/showInfo/ShowInfo.styled.ts
+++ src/pages/gig/components/showInfo/ShowInfo.styled.ts
@@ -1,3 +1,4 @@
+import { ButtonDelete24 } from "@assets/svgs";
 import { Generators } from "@styles/generator";
 import styled from "styled-components";
 
@@ -12,7 +13,36 @@ export const Poster = styled.img`
   object-fit: cover;
   object-position: center;
 
+  cursor: pointer;
   border-radius: 4px;
+`;
+
+export const ModalOverlay = styled.div`
+  position: fixed;
+  inset: 0;
+  z-index: 10;
+  ${Generators.flexGenerator("column")};
+
+  background-color: rgb(15 15 15 / 70%);
+`;
+
+export const CloseBtn = styled.section`
+  position: absolute;
+  top: 1.6rem;
+  width: 37rem;
+
+  ${Generators.flexGenerator("row", "center", "end")};
+  cursor: pointer;
+`;
+
+export const CloseIcon = styled(ButtonDelete24)`
+  width: 2.4rem;
+  height: 2.4rem;
+  margin-right: 1.3rem;
+`;
+
+export const ModalContent = styled.img`
+  max-width: 37rem;
 `;
 
 export const Title = styled.h1`
