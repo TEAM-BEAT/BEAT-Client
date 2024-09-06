e3cdee9e82132827d1955a16062bb46e810135aa
jerry <becomegoodguy@gmail.com>
2024-09-06 17:28:32 +0900

    test: replace head

--- vite.config.ts
+++ vite.config.ts
@@ -43,25 +43,21 @@ export default defineConfig(async ({ mode }) => {
 
           if (context.route.includes("/gig")) {
             const response = await axios.get(
-              `https://api.sinjibabo.shop/api/performances/detail/${context.route.slice(context.route.lastIndexOf("/") + 1)}`
+              `${env.VITE_API_BASE_URL}/performances/detail/${context.route.slice(context.route.lastIndexOf("/") + 1)}`
             );
 
             const performanceData = response.data.data;
 
-            context.html = `
-            <!DOCTYPE html>
-            <html>
-              <head>
+            context.html = context.html.replace(
+              /<\/head>/i,
+              `
                 <meta property="og:title" content="${performanceData.performanceTitle || "BEAT"}" />
                 <meta property="og:image" content="${performanceData.posterImage || "https://www.sinjibabo.shop/og_img.png"}" />
                 <meta name="keywords" content="공연, 밴드, 뮤지컬, 비트, beat" />
                 <meta property="og:description" content="${performanceData.performanceDescription || "심장이 뛰는 곳, BEAT에서 만나보세요."}" />
                 <meta property="og:url" content="https://www.sinjibabo.shop/gig/${performanceData.performanceId}" />
-              </head>
-              <body>
-              </body>
-            </html>
-          `;
+              </head>`
+            );
           }
         },
       }),
