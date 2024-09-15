5e06ec39ce0f7b70029a354a45ee942e4d44d1b9
Sinji <sinzi031012@naver.com>
2024-09-16 02:45:18 +0900

    feat: 어드민 페이지 네비게이션 바 생성

--- src/components/layout/AdminLayout.tsx
+++ src/components/layout/AdminLayout.tsx
@@ -1,13 +1,23 @@
 import { Outlet } from "react-router-dom";
+import styled from "styled-components";
 import AdminHeader from "@admin/compontets/adminHeader/AdminHeader";
+import AdminNavBar from "@admin/compontets/adminNavBar/AdminNavBar";
 
 const AdminLayout = () => {
   return (
     <div>
       <AdminHeader />
-      <Outlet />
+      <Admin>
+        <AdminNavBar />
+        <Outlet />
+      </Admin>
     </div>
   );
 };
 
 export default AdminLayout;
+
+const Admin = styled.section`
+  display: flex;
+  flex-direction: row;
+`;
--- src/routes/AdminRoutes.tsx
+++ src/routes/AdminRoutes.tsx
@@ -1,9 +1,24 @@
 import Promotion from "@admin/pages/promotion/Promotion";
+import User from "@admin/pages/user/User";
+import Gig from "@admin/pages/gig/Gig";
+import Book from "@admin/pages/booking/Book";
 
 const ADMIN_ROUTES = [
   {
     path: "promotion",
     element: <Promotion />,
+  },
+  {
+    path: "user",
+    element: <User />,
+  },
+  {
+    path: "gig",
+    element: <Gig />,
+  },
+  {
+    path: "book",
+    element: <Book />,
   },
 ];
 
