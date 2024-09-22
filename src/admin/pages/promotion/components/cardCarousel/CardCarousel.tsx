21f6fef3b05083c984eccb47d5a7377feba3f339
Sinji <sinzi031012@naver.com>
2024-09-22 13:30:07 +0900

    feat: 이미지 업로드 기능 추가

--- src/admin/pages/promotion/components/cardCarousel/CardCarousel.tsx
+++ src/admin/pages/promotion/components/cardCarousel/CardCarousel.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useState, useRef, ChangeEvent, useEffect } from "react";
 import * as S from "./CardCarousel.styled";
 import { Spacing } from "@components/commons";
 import AdminButton from "@admin/compontets/commons/adminButton/AdminButton";
@@ -6,12 +6,33 @@ import LinkButton from "@admin/compontets/commons/linkButton/LinkButton";
 
 const CardCarousel = ({ index, carouselImg, redirectUrl, deleteCarousel, handleLinkModal }) => {
   const ref = useRef<HTMLInputElement>(null);
+  const [postImg, setPostImg] = useState<File | null>(null);
+  const [imgUrl, setImgUrl] = useState(carouselImg);
 
   const fileHandler = () => {
     if (ref.current) {
       ref.current!.click();
     }
   };
+
+  useEffect(() => {
+    setImgUrl(carouselImg || null);
+  }, [carouselImg]);
+
+  const uploadFile = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const fileReader = new FileReader();
+      fileReader.onload = function (event) {
+        const newImageUrl = event.target?.result as string;
+        setPostImg(file);
+        setImgUrl(newImageUrl);
+      };
+
+      fileReader.readAsDataURL(file);
+    }
+  };
+
   return (
     <S.CardCarouselWrapper>
       <S.SwitchIcon />
@@ -21,8 +42,8 @@ const CardCarousel = ({ index, carouselImg, redirectUrl, deleteCarousel, handleL
             deleteCarousel(index);
           }}
         />
-        {carouselImg ? (
-          <img src={carouselImg} alt="carousel-img" />
+        {imgUrl ? (
+          <img src={imgUrl} alt="carousel-img" />
         ) : (
           <S.EmptyImg>
             <S.ImgIcon />
@@ -35,6 +56,7 @@ const CardCarousel = ({ index, carouselImg, redirectUrl, deleteCarousel, handleL
           ref={ref}
           accept="image/png, image/jpg, image/jpeg, image/svg"
           style={{ display: "none" }}
+          onChange={uploadFile}
         />
         {carouselImg ? "이미지 변경" : "이미지 등록"}
       </AdminButton>
