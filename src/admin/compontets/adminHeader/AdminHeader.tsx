7ff1c8aa421295ad05f73099b4d40324e9d357ac
Sinji <sinzi031012@naver.com>
2024-09-30 19:16:35 +0900

    feat: 접근 권한 설정 기능 구현

--- src/admin/compontets/adminHeader/AdminHeader.tsx
+++ src/admin/compontets/adminHeader/AdminHeader.tsx
@@ -1,6 +1,19 @@
+import { useEffect } from "react";
 import * as S from "./AdminHeader.styled";
+import { useNavigate } from "react-router-dom";
 
 function AdminHeader() {
+  const navigate = useNavigate();
+  const role = localStorage?.getItem("ROLE_ADMIN");
+
+  useEffect(() => {
+    const userObj = JSON.parse(role);
+
+    if (userObj === null) {
+      navigate("/admin/notfound");
+    }
+  }, []);
+
   return (
     <S.AdminHeaderWrapper>
       <S.LogoIcon />
