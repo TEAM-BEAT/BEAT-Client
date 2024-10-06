a06f6f0c65d9cd9ab2061c8e297462d97b665d1d
imddoy <imddoy@duksung.ac.kr>
2024-10-06 17:31:53 +0900

    feat: 에디터 기능 추가

--- src/pages/modifyManage/components/RoleWrapper.tsx
+++ src/pages/modifyManage/components/RoleWrapper.tsx
@@ -4,6 +4,7 @@ import Spacing from "@components/commons/spacing/Spacing";
 import { nameFilter } from "@utils/useInputFilter";
 import { ChangeEvent, useState } from "react";
 import * as S from "../ModifyManage.styled";
+import ImageEditor from "@components/commons/imageEditor/ImageEditor";
 
 interface Role {
   makerId: number;
@@ -23,6 +24,7 @@ const RoleWrapper = ({ id, role, removeRole, onUpdateRole }: RoleWrapperProps) =
   const { makerName, makerRole, makerPhoto } = role;
   const [postImg, setPostImg] = useState<File | null>(null);
   const [previewImg, setPreviewImg] = useState<string | null>(makerPhoto || null);
+  const [openImageModal, setOpenImageModal] = useState(false);
 
   const uploadFile = (e: ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
@@ -34,6 +36,7 @@ const RoleWrapper = ({ id, role, removeRole, onUpdateRole }: RoleWrapperProps) =
         const imageUrl = event.target?.result as string;
         setPreviewImg(imageUrl);
         onUpdateRole(id, "makerPhoto", imageUrl);
+        setOpenImageModal(true);
       };
 
       fileReader.readAsDataURL(file);
@@ -45,52 +48,64 @@ const RoleWrapper = ({ id, role, removeRole, onUpdateRole }: RoleWrapperProps) =
     onUpdateRole(id, name, value);
   };
 
+  // ImageEditor에서 크롭된 이미지 URL을 받아서 상태 업데이트
+  const handleCroppedImage = (croppedImageUrl: string) => {
+    setPreviewImg(croppedImageUrl); // 프리뷰 이미지 업데이트
+    onUpdateRole(id, "makerPhoto", croppedImageUrl); // makerPhoto 업데이트
+    setOpenImageModal(false); // 모달 닫기
+  };
+
   return (
-    <S.RoleWrapper>
-      <S.FileInputWrapper>
-        {previewImg ? (
-          <S.PreviewImageWrapper width={13.6} height={15.8}>
-            <S.PreviewImage src={previewImg} alt="Preview" width={13.6} height={15.8} />
-            <S.RemoveImageButton onClick={() => removeRole(id)} />
-          </S.PreviewImageWrapper>
-        ) : (
-          <>
-            <S.HiddenFileInput
-              type="file"
-              id={`file-${id}`}
-              onChange={uploadFile}
-              disabled={false}
-            />
-            <S.CustomFileInput htmlFor={`file-${id}`} width={13.6} height={15.8}>
-              <IconCamera width={"3.2rem"} />
+    <>
+      <S.RoleWrapper>
+        <S.FileInputWrapper>
+          {previewImg ? (
+            <S.PreviewImageWrapper width={13.6} height={15.8}>
+              <S.PreviewImage src={previewImg} alt="Preview" width={13.6} height={15.8} />
               <S.RemoveImageButton onClick={() => removeRole(id)} />
-            </S.CustomFileInput>
-          </>
-        )}
-      </S.FileInputWrapper>
-      <Spacing marginBottom="1.6" />
-      <S.TextInputWrpper>
-        <TextField
-          type="input"
-          name="makerName"
-          value={makerName}
-          onChange={handleInputChange}
-          filter={nameFilter}
-          narrow={true}
-          placeholder="이름"
-          disabled={false}
-        />
-        <TextField
-          type="input"
-          name="makerRole"
-          value={makerRole}
-          onChange={handleInputChange}
-          narrow={true}
-          placeholder="역할"
-          disabled={false}
-        />
-      </S.TextInputWrpper>
-    </S.RoleWrapper>
+            </S.PreviewImageWrapper>
+          ) : (
+            <>
+              <S.HiddenFileInput
+                type="file"
+                id={`file-${id}`}
+                onChange={uploadFile}
+                disabled={false}
+              />
+              <S.CustomFileInput htmlFor={`file-${id}`} width={13.6} height={15.8}>
+                <IconCamera width={"3.2rem"} />
+                <S.RemoveImageButton onClick={() => removeRole(id)} />
+              </S.CustomFileInput>
+            </>
+          )}
+        </S.FileInputWrapper>
+        <Spacing marginBottom="1.6" />
+        <S.TextInputWrpper>
+          <TextField
+            type="input"
+            name="makerName"
+            value={makerName}
+            onChange={handleInputChange}
+            filter={nameFilter}
+            narrow={true}
+            placeholder="이름"
+            disabled={false}
+          />
+          <TextField
+            type="input"
+            name="makerRole"
+            value={makerRole}
+            onChange={handleInputChange}
+            narrow={true}
+            placeholder="역할"
+            disabled={false}
+          />
+        </S.TextInputWrpper>
+      </S.RoleWrapper>
+      {openImageModal && (
+        <ImageEditor file={previewImg} aspectRatio={1} onCropped={handleCroppedImage} />
+      )}
+    </>
   );
 };
 
