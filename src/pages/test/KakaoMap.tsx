de034e706df36d3d4db78c8bd3c276a363655a8c
ocahs9 <piorag99@gmail.com>
2024-11-20 04:22:03 +0900

    feat: 지도 기능 테스트(위치 조정, bound, 검색, 마커 커스텀)

--- src/pages/test/KakaoMap.tsx
+++ src/pages/test/KakaoMap.tsx
@@ -1,39 +1,106 @@
 import React, { useEffect, useState } from "react";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
+import banner_basic from "@assets/images/banner_basic.png";
+import icon_toss_svg from "../../../public/svgs/icon_toss.svg";
+
+type Position = {
+  lat: number;
+  lng: number;
+};
+
+type Marker = {
+  position: Position;
+  content: string;
+};
 
 const KakaoMap = () => {
   const { kakao } = window;
-  const [isLoaded, setIsLoaded] = useState(false);
+  const [info, setInfo] = useState<Marker | null>(null);
+  const [markers, setMarkers] = useState<Marker[]>([
+    {
+      position: {
+        lat: 37.5494796485918,
+        lng: 126.924854360345,
+      },
+      content: "테스트",
+    },
+  ]);
+  const [map, setMap] = useState<typeof kakao.maps.Map | null>(null);
+  const [location, setLocation] = useState<Position>({ lat: 37.566826, lng: 126.9786567 });
 
   useEffect(() => {
-    setIsLoaded(true); //다음 번에는 바로 로딩이 되도록 해당 위치에 정의
-    if (!isLoaded) {
-      //이래야 getElementById가 정상적으로 작동함
+    if (!map) {
       return;
     }
-    // 카카오 지도 객체 생성 예제
-    const container = document.getElementById("map"); // 지도를 표시할 div
-    const options = {
-      center: new kakao.maps.LatLng(37.5665, 126.978), // 초기 위치
-      level: 3, // 초기 확대 레벨
-    };
+    const ps = new kakao.maps.services.Places();
 
-    const map = new kakao.maps.Map(container, options);
+    // ps.keywordSearch("홍익대학교 제2기숙사", (data, status, _pagination) => {
+    //   if (status === kakao.maps.services.Status.OK) {
+    //     // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+    //     // LatLngBounds 객체에 좌표를 추가합니다
+    //     const bounds = new kakao.maps.LatLngBounds();
+    //     console.log("bounds:", bounds);
+    //     const markers = [];
 
-    // // 지도에 마커 추가
-    const marker = new kakao.maps.Marker({
-      position: new kakao.maps.LatLng(37.5665, 126.978),
-    });
-    marker.setMap(map);
-  }, [isLoaded]);
+    //     for (var i = 0; i < data.length; i++) {
+    //       console.log(data[i]);
+    //       // @ts-ignore
+    //       markers.push({
+    //         position: {
+    //           lat: data[i].y,
+    //           lng: data[i].x,
+    //         },
+    //         content: data[i].place_name,
+    //       });
+    //       // @ts-ignore
+    //       bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+    //     }
+    //     setMarkers(markers);
+
+    //     // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+    //     map.setBounds(bounds);
+    //   }
+    // });
+
+    //테스트로, 처음 위치 직접 지정
+    setLocation({ lat: 37.5494796485918, lng: 126.924854360345 });
+  }, [map]);
 
   return (
-    <div>
-      {isLoaded ? (
-        <div id="map" style={{ width: "100%", height: "400px" }}></div>
-      ) : (
-        <p>Loading map...</p>
-      )}
-    </div>
+    <Map // 로드뷰를 표시할 Container
+      center={{
+        lat: location.lat,
+        lng: location.lng,
+      }}
+      style={{
+        width: "100%",
+        height: "350px",
+      }}
+      level={3}
+      onCreate={setMap}
+    >
+      {markers.map((marker) => {
+        console.log(marker);
+        return (
+          <MapMarker
+            key={`marker-${marker.content}-${marker.position.lat},${marker.position.lng}`}
+            position={marker.position}
+            onClick={() => setInfo(marker)}
+            image={{
+              src: icon_toss_svg,
+              size: {
+                width: 30,
+                height: 30,
+              },
+            }}
+          >
+            {info && info.content === marker.content && (
+              <div style={{ color: "#000" }}>{marker.content}</div>
+            )}
+          </MapMarker>
+        );
+      })}
+    </Map>
   );
 };
 
