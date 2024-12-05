ad2ecbe037c5cb638ec8a597eac6ee4b13e7c990
imddoy <imddoy@duksung.ac.kr>
2024-12-06 05:18:52 +0900

    style: 판매자 기준 예매자 목록 쿼리키 변경

--- src/apis/domains/tickets/queries.ts
+++ src/apis/domains/tickets/queries.ts
@@ -8,16 +8,14 @@ import {
   TicketUpdateRequest,
 } from "./api";
 
-import { BOOKING_QUERY_KEY } from "../bookings/queries";
-
 // 예매자 목록 조회 API (GET)를 위한 쿼리 작성
 const QUERY_KEY = {
-  LIST: "list",
+  SELLER_BOOKING_LIST: "sellerBookingList",
 };
 
 export const useTicketRetrive = (formData: getTicketReq) => {
   return useQuery({
-    queryKey: [QUERY_KEY.LIST, BOOKING_QUERY_KEY.BOOKING_LIST],
+    queryKey: [QUERY_KEY.SELLER_BOOKING_LIST],
     queryFn: () => getTicketRetrieve(formData),
     // staleTime: 1000 * 60 * 60,
     gcTime: 1000 * 60 * 60 * 24,
@@ -31,8 +29,8 @@ export const useTicketUpdate = () => {
   return useMutation({
     mutationFn: (formData: TicketUpdateRequest) => putTicketUpdate(formData),
     onSuccess: (res) => {
-      queryClient.invalidateQueries({ queryKey: [QUERY_KEY.LIST, BOOKING_QUERY_KEY.BOOKING_LIST] });
-      queryClient.refetchQueries({ queryKey: [QUERY_KEY.LIST, BOOKING_QUERY_KEY.BOOKING_LIST] });
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEY.SELLER_BOOKING_LIST] });
+      queryClient.refetchQueries({ queryKey: [QUERY_KEY.SELLER_BOOKING_LIST] });
     },
   });
 };
@@ -44,8 +42,8 @@ export const useTicketPatch = () => {
   return useMutation({
     mutationFn: (formData: PatchFormDataProps) => patchTicketCancel(formData),
     onSuccess: (res) => {
-      queryClient.invalidateQueries({ queryKey: [QUERY_KEY.LIST, BOOKING_QUERY_KEY.BOOKING_LIST] });
-      queryClient.refetchQueries({ queryKey: [QUERY_KEY.LIST, BOOKING_QUERY_KEY.BOOKING_LIST] });
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEY.SELLER_BOOKING_LIST] });
+      queryClient.refetchQueries({ queryKey: [QUERY_KEY.SELLER_BOOKING_LIST] });
     },
   });
 };
