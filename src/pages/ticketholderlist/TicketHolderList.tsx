0859cef3d4c51a59ded68132d8bd29420c04580c
Sinji <sinzi031012@naver.com>
2025-01-04 18:02:33 +0900

    fix: 아이폰 인앱브라우저 오픈 시 시스템 브라우저로 열도록 변경

--- src/pages/ticketholderlist/TicketHolderList.tsx
+++ src/pages/ticketholderlist/TicketHolderList.tsx
@@ -389,7 +389,8 @@ const TicketHolderList = () => {
       const targetUrl = "https://www.beatlive.kr/gig-manage";
 
       if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
-        window.location.href = "x-web-https://www.beatlive.kr/gig-manage";
+        // window.location.href = `x-web-search://${targetUrl}`;
+        window.open("https://www.beatlive.kr/gig-manage", "_system");
       } else {
         window.location.href = `intent://${targetUrl.replace(
           /https?:\/\//i,
