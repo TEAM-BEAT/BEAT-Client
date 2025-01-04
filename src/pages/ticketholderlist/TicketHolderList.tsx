78d96ffe789db30427e7c097be16e70b3cfe73c8
Sinji <sinzi031012@naver.com>
2025-01-04 17:47:40 +0900

    fix: 인앱브라우저 사파리 이동 시 검색으로 이동되는 오류 수정

--- src/pages/ticketholderlist/TicketHolderList.tsx
+++ src/pages/ticketholderlist/TicketHolderList.tsx
@@ -389,7 +389,7 @@ const TicketHolderList = () => {
       const targetUrl = "https://www.beatlive.kr/gig-manage";
 
       if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
-        window.location.href = `x-web-search://?${targetUrl}`;
+        window.location.href = `x-web://?${targetUrl}`;
       } else {
         window.location.href = `intent://${targetUrl.replace(
           /https?:\/\//i,
