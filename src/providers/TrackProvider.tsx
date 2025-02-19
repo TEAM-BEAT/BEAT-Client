6f6ad4e028b6eea1b8106c0d2ed69cdc9ce1295d
Sinji <sinzi031012@naver.com>
2025-02-19 19:35:54 +0900

    feat: 믹스패널 프로바이더 세팅

---
+++ src/providers/TrackProvider.tsx
@@ -0,0 +1,14 @@
+import { track } from "../track";
+import React from "react";
+
+interface TrackProviderProps {
+  children: React.ReactNode;
+}
+
+export default function TrackProvider({ children }: TrackProviderProps) {
+  React.useEffect(() => {
+    track.initialize();
+  }, []);
+
+  return <>{children}</>;
+}
