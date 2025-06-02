57bff050fce17f6e5e8b848f3b7610efb81fb4c3
Sinji <sinzi031012@naver.com>
2025-06-02 15:54:33 +0900

    fix: 프레이머 연결을 위해 main 리다이렉팅 연결 해제

--- src/pages/onBoarding/OnBoarding.tsx
+++ src/pages/onBoarding/OnBoarding.tsx
@@ -1,13 +1,4 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-
 const OnBoarding = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    navigate("/main");
-  }, []);
-
   return null;
 };
 
