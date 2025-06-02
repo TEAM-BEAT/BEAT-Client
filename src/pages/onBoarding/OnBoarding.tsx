97a51abb5c1de843db8c37e9e8d9bb2e8c668fea
Sinji <sinzi031012@naver.com>
2025-06-02 17:30:50 +0900

    fix: export 오류 수정

--- src/pages/onBoarding/OnBoarding.tsx
+++ src/pages/onBoarding/OnBoarding.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-const OnBoarding = () => {
+export const OnBoarding = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
