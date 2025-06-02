60656db294dabdc3580420da82990cd7753690e6
Sinji <sinzi031012@naver.com>
2025-06-02 17:19:28 +0900

    fix: main 리다이렉팅 재연결

--- src/pages/onBoarding/OnBoarding.tsx
+++ src/pages/onBoarding/OnBoarding.tsx
@@ -1,5 +1,12 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 const OnBoarding = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/main");
+  }, []);
+
   return null;
 };
-
-export default OnBoarding;
