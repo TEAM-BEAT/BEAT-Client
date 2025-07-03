361c3a165b1d5bca001fc980245dc0b1c2b4d1bc
imddoy <imddoy@duksung.ac.kr>
2025-07-03 19:03:11 +0900

    fix: 예매자 목록 순서 정렬 안되는 이슈 해결

--- src/pages/ticketholderlist/TicketHolderList.tsx
+++ src/pages/ticketholderlist/TicketHolderList.tsx
@@ -368,15 +368,12 @@ const TicketHolderList = () => {
           return scheduleDiff;
         }
 
-        const createdAtDiff = new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime();
-        if (createdAtDiff !== 0) {
-          return createdAtDiff;
-        }
-
         const statusDiff = a.bookingStatus.localeCompare(b.bookingStatus, "ko");
         if (statusDiff !== 0) {
           return statusDiff;
         }
+
+        return a.createdAt.localeCompare(b.createdAt, "ko");
       });
 
       setCSVDataArr(tempCSVDataArr);
