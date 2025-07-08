19a99379157ddc0e8963a880c464a697ce4ae66e
imddoy <imddoy@duksung.ac.kr>
2025-07-09 00:20:44 +0900

    chore: 필요없는 경로 삭제

--- src/utils/generatePerformanceRoute.ts
+++ src/utils/generatePerformanceRoute.ts
@@ -36,7 +36,7 @@ async function fetchPerformanceIds(url: string) {
 
 // 사전 렌더링 경로 생성
 export async function generatePerformanceRoutes(url: string) {
-  const staticRoutes = ["/gig/7", "/main"]; // 정적 경로
+  const staticRoutes = ["/", "/main"]; // 정적 경로
 
   const dynamicRoutes = await fetchPerformanceIds(url); // 동적 경로 가져오기
 
