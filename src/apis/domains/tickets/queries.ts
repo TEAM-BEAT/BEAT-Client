0726c3c6f2dfd44a56bc0a604127d76be925bf2b
imddoy <imddoy@duksung.ac.kr>
2026-05-30 02:10:37 +0900

    chore: 안쓰는 queryClient import 삭제

--- src/apis/domains/tickets/queries.ts
+++ src/apis/domains/tickets/queries.ts
@@ -1,4 +1,4 @@
-import { QueryClient, useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import {
   getTicketReq,
   getTicketRetrieve,
